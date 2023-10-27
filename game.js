2f61442ad86fe04294417b6dbdb0a2e8f97eb26f
JulioNunesDev <78341732+JulioNunesDev@users.noreply.github.com>
2023-10-27 20:48:15 -0300

    fisrt my commit

---
+++ game.js
@@ -0,0 +1,18 @@
+import { Canvas } from "./content/Array"
+//Importações de Paths
+
+
+let boxCss = 'boxCss'
+
+class Teste {
+
+ constructor(width, height) {
+
+}
+
+
+
+
+}
+
+const mygame = new Teste(window.innerWidth, window.innerHeight)
